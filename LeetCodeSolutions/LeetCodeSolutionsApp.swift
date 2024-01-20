c0ad616e2c5afb59be0895c308a171f7eda75bf5
German <lordireality@gmail.com>
2024-01-20 12:50:57 +0300

    Initial Commit

---
+++ LeetCodeSolutions/LeetCodeSolutionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCodeSolutionsApp.swift
+//  LeetCodeSolutions
+//
+//  Created by Герман Зыкин on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct LeetCodeSolutionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
