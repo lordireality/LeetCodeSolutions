6a4dec9bfc822dc7def7d7acec5622ff8289dc0e
German <lordireality@gmail.com>
2024-01-27 18:52:40 +0300

    374

---
+++ LeetCodeSolutions/SolutionSources/374.swift
@@ -0,0 +1,27 @@
+/**
+ * Forward declaration of guess API.
+ * @param  num -> your guess number
+ * @return          -1 if num is higher than the picked number
+ *                  1 if num is lower than the picked number
+ *               otherwise return 0
+ * func guess(_ num: Int) -> Int
+ */
+
+class Solution : GuessGame {
+    func guessNumber(_ n: Int) -> Int {
+        var low = 0
+        var high = n
+        while low <= high {
+            let mid = (low+high) / 2
+            if guess(mid) == 0 {
+                return mid
+            }
+            if guess(mid) == 1 {
+                low = mid + 1
+            } else if guess(mid) == -1 {
+                high = mid - 1
+            }
+        }
+        return -1
+    }
+}
